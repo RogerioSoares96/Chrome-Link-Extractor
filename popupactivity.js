91b13449ca30c2dd66c55ef7eb7fbb740e33f212
Rogerio Soares <rogeryu_gap@hotmail.com>
2022-09-04 19:45:05 +0100

    Typo Commit

--- popupactivity.js
+++ popupactivity.js
@@ -6,7 +6,6 @@ activeButton.addEventListener('input', async function(){
     if (activeButton.value === "off") {
      activeButton.value = "on";
      chrome.omnibox.addEventListener('hashchange', )
-     // https://computerrock.com/en/blog/html5-changing-the-browser-url-without-refreshing-page
     };
     if (activeButton.value === "on") {
         activeButton.value = "off";
